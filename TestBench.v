1b13f2a49f1da804f42641a4719d4b8966ce4ede
ldnelson16 <ldnelson16@gmail.com>
2024-06-19 11:46:21 -0400

    TestBench tests ring buffer UART_ControlleR

--- TestBench.v
+++ TestBench.v
@@ -10,27 +10,59 @@ module TB_Controller_Simulator ();
   // Declare signals
   reg clk;
   reg rst;
-  reg rx;
-  wire tx;
+  reg rx1; reg rx2;
+  wire tx1; wire tx2;
 
   // Modules
-  UART_CONTROLLER #(.WORD_SIZE(params.WORD_SIZE), .BAUD_RATE(params.BAUD_RATE), .CLOCK_FREQ(params.CLOCK_FREQ)) uart_controller(
+  UART_CONTROLLER uart_controller_1(
     .clk(clk),
     .rst(rst),
-    .rx(rx),
-    .tx(tx)
+    .rx(rx1),
+    .tx(tx1)
+  );
+  UART_CONTROLLER uart_controller_2(
+    .clk(clk),
+    .rst(rst),
+    .rx(rx2),
+    .tx(tx2)
   );
 
   initial begin
     // clock
     clk <= 0;
-    forever #10 clk = ~clk; // 50 MHz
+    forever #10 clk = ~clk; // 20 ns cycle // 50 MHz
   end
 
-  initial begin
-    rx <= 1; #1000
-    rx <= 0;
-  end
+  integer i,j; // for loop
+
+  // Simulate rx;
+  initial begin : simulator
+    // CORRECT BYTE
+    rx1 <= 1; #(params.BAUD*6); // IDLE
+    for (j=0; j < 14; j = j + 1) begin
+      rx1 <= 0; #(params.BAUD); // START BIT
+
+      for (i = 0; i < 8; i = i + 1) begin
+        rx1 <= (3*i*j + i + (j+1)**2 + j + j**2 + (2*i)**3) % 2; // Alternate between 0 and 1
+        #(params.BAUD); // 1 baud cycle per bit
+      end
+
+      rx1 <= 1; #(params.BAUD); // END BIT
+      rx1 <=1; #(params.BAUD*(0.25*j)); // IDLE
+    end
+
+    // FAIL BYTE (DUE TO END BIT)
+    rx1 <= 1; #(params.BAUD*6); // IDLE
+    rx1 <= 0; #(params.BAUD); // START BIT
+
+    for (i = 0; i < 8; i = i + 1) begin
+      rx1 <= i % 2; // Alternate between 0 and 1
+      #(params.BAUD); // 1 baud cycle per bit
+    end
+
+    rx1 <= 0; #(params.BAUD); // ERROR! END BIT
+    rx1 <=1; #(params.BAUD*6); // IDLE
+  end : simulator
 
 endmodule
 
