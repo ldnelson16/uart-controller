4edeea9a6014cfa5158ef2e9a3af5e60fa88b7b0
Luke Nelson <51863968+ldnelson16@users.noreply.github.com>
2024-07-05 17:12:52 -0400

    Update UART_OS_simulator.v
    OS can send data input via buttons.

--- UART_OS_simulator.v
+++ UART_OS_simulator.v
@@ -10,16 +10,38 @@ module UART_OS_simulator
     output reg[7:0] send_data_to_nic;
     output reg write_nic,
     output reg read_nic,
-    output reg[7:0] D0,D1,D2,
+    output[7:0] D0,D1,D2,
     output[3:0] LEDR_for_num_data_rcvd
   );
 
   reg[7:0] data_display;
   reg[3:0] num_data_received;
 
-  // Read from NIC logic // and increment num_data_received
+  initial begin
+    data_display <= 0;
+    num_data_received <= 0;
+    write_nic <= 0;
+    read_nic <= 0;
+  end
 
+  // Read from NIC logic // and increment num_data_received
+  if (send_data_i) begin
+    read_nic <= 1;
+  end else begin
+    read_nic <= 0;
+  end
+  if (read_nic) begin
+    data_display <= rec_data_from_nic;
+    num_data_received = (num_data_received + 1);
+  end
+  
   // Write to NIC logic
+  if (send_button) begin
+    send_data_to_nic <= send_data_from_buttons;
+    write_nic <= 1;
+  end else begin
+    write_nic <= 0;
+  end
 
   // Display logic using Bin-> 7seg
   Binary_2_7SEG display_data_sent_from_1(
