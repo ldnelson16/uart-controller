c872301cd5e2fc7c96a93776e5e064d127daa1ff
ldnelson16 <ldnelson16@gmail.com>
2024-07-06 23:30:17 -0400

    button resets itself for 0.5 seconds

--- UART_OS_simulator.v
+++ UART_OS_simulator.v
@@ -1,17 +1,16 @@
 module UART_OS_simulator
-  #(
-
-  )
   (
+    input wire clk,
     input wire send_button,
     input wire[7:0] send_data_from_buttons,
     input wire[7:0] rec_data_from_nic,
     input wire send_data_i,
-    output reg[7:0] send_data_to_nic;
+    input wire editing_input_data,
+    output reg[7:0] send_data_to_nic,
     output reg write_nic,
     output reg read_nic,
-    output[7:0] D0,D1,D2,
-    output[3:0] LEDR_for_num_data_rcvd
+    output [6:0] D0,D1,D2,
+    output reg [3:0] LEDR_for_num_data_rcvd
   );
 
   reg[7:0] data_display;
@@ -22,25 +21,38 @@ module UART_OS_simulator
     num_data_received <= 0;
     write_nic <= 0;
     read_nic <= 0;
+    send_data_to_nic <= 0;
   end
 
   // Read from NIC logic // and increment num_data_received
-  if (send_data_i) begin
-    read_nic <= 1;
-  end else begin
-    read_nic <= 0;
+  always @ (posedge clk) begin
+    if (send_data_i) begin
+      read_nic <= 1;
+    end else begin
+      read_nic <= 0;
+    end
+    if (read_nic) begin
+      num_data_received = (num_data_received + 1);
+    end
   end
-  if (read_nic) begin
-    data_display <= rec_data_from_nic;
-    num_data_received = (num_data_received + 1);
-  end
-  
+
   // Write to NIC logic
-  if (send_button) begin
-    send_data_to_nic <= send_data_from_buttons;
-    write_nic <= 1;
-  end else begin
-    write_nic <= 0;
+  always @ (posedge clk) begin
+    if (send_button) begin
+      send_data_to_nic <= send_data_from_buttons;
+      write_nic <= 1;
+    end else begin
+      write_nic <= 0;
+    end
+  end
+
+  // Logic for what to display
+  always @ (posedge clk) begin
+    if (send_button) begin
+      data_display <= send_data_from_buttons;
+    end else if (read_nic) begin
+      data_display <= rec_data_from_nic;
+    end
   end
 
   // Display logic using Bin-> 7seg
@@ -52,6 +64,8 @@ module UART_OS_simulator
   );
 
   // Display logic for counters
-  assign LEDR_for_num_data_rcvd = num_data_received;
+  always @ (posedge clk) begin 
+    LEDR_for_num_data_rcvd <= num_data_received;
+  end
 
 endmodule
